33444b56e2294a33d3b74fa3cc3444e85f742921
Jackiepie <lfw8517@gmail.com>
2016-02-05 21:35:20 +0800

    Initial Commit

---
+++ Out of This World/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Out of This World
+//
+//  Created by Jackie Lee on 16/2/5.
+//  Copyright © 2016年 LFW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Out of This WorldTests/Out_of_This_WorldTests.m
@@ -0,0 +1,39 @@
+//
+//  Out_of_This_WorldTests.m
+//  Out of This WorldTests
+//
+//  Created by Jackie Lee on 16/2/5.
+//  Copyright © 2016年 LFW. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Out_of_This_WorldTests : XCTestCase
+
+@end
+
+@implementation Out_of_This_WorldTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
